39fcbbea54a4cd8d9ddd29a2b65d249077994fc9
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-27 09:03:18 +0100

    Done with retrieving images from the pokemon api to gridtiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex/bloc/pokemon_event.dart';
+import 'package:pokedex/pokedex_view.dart';
+
+import 'bloc/pokemon_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +17,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: Theme.of(context).copyWith(
+          primaryColor: Colors.red,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.redAccent)),
+      home: MultiBlocProvider(providers: [
+        BlocProvider(
+            create: (context) =>
+                PokemonBloc()..add(PokemonPageRequest(page: 0)))
+      ], child: PokedexView()),
+    );
   }
 }
